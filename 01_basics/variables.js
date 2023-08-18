f65fed6c7e0a894c1c04e599dc3e540607182514
Ambikasabhlock <137787274+Ambikasabhlock@users.noreply.github.com>
2023-08-18 08:30:17 +0000

    configured

---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+const accountId=1333;
+let accountName="tina";
+var email="tina@gm.com"
+var  accountPassword= "12344234";
+accountCity="Pune";//like this also can store variable but not good practice.
+let accountState;
+
+// accountId=23;// not allwed because is in const key
+
+accountName ="Aerina"
+email="arena2gm.com"
+accountPassword="125678ad"
+accountCity="delhi"
+
+console.table([accountId,accountName,email,accountPassword,accountState]);
+
+//console.log(accountId);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope.
+*/
